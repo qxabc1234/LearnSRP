08f073f49ef006217dc6327ff23d9ffc4bd5b132
trouger <trouger@126.com>
2024-06-28 14:59:12 +0800

    first commit

---
+++ Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline 
+{
+    protected override void Render(
+    ScriptableRenderContext context, Camera[] cameras
+)
+    { }
+}
